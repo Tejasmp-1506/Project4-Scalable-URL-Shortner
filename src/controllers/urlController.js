1678d001acba5d29f48e4fde691fe568393751c7
Tejasmp-1506 <tejaspatil22121@gmail.com>
2022-04-19 12:24:00 +0530

    nothing

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,15 +2,15 @@ const urlModel = require("../Models/urlModel");
 const shortid = require("short-id");
 const redis = require("redis");
 
-const { promisify } = require("util"); //--------->
+const { promisify } = require("util");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  16368,
-  "redis-16368.c15.us-east-1-2.ec2.cloud.redislabs.com",
+  19918,
+  "redis-19918.c212.ap-south-1-1.ec2.cloud.redislabs.com",
   { no_ready_check: true }
 );
-redisClient.auth("Y52LH5DG1XbiVCkNC2G65MvOFswvQCRQ", function (err) {
+redisClient.auth("RFvQ3fwtJ2FG5ui7TvMCectx14LLWAii", function (err) {
   if (err) throw err;
 });
 
@@ -21,28 +21,15 @@ redisClient.on("connect", async function () {
 //1. connect to the server
 //2. use the commands :
 
-//Connection setup for redis
+//Connection setup for redis------------------
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
-// Link : redis-16368.c15.us-east-1-2.ec2.cloud.redislabs.com
-
-// Port Number : 16368
-// Password : Y52LH5DG1XbiVCkNC2G65MvOFswvQCRQ
-
-
 const urlShortner = async (req, res) => {
   try {
-    let baseUrl = "http://localhost:3000";
-    let urlCode = shortid.generate();
-    let body = req.body;
-    let longUrl = body.longUrl;
-
-    let shortUrl = baseUrl + "/" + urlCode;
-    let data = { urlCode, longUrl, shortUrl };
-
     // Validation for BaseUrl :
+    let baseUrl = "http://localhost:3000";
     if (!/^https?:\/\/\w/.test(baseUrl)) {
       return res
         .status(400)
@@ -52,81 +39,98 @@ const urlShortner = async (req, res) => {
         });
     }
 
-    if (Object.keys(body).length == 0) {
-      return res
-        .status(400)
-        .send({ status: false, message: "req body should have some data" });
-    }
-
+    // UrlCode Generate :
+    let urlCode = shortid.generate();
     // Validation for Long Url :
+    let longUrl = req.body.longUrl;
     if (!longUrl) {
       return res
         .status(400)
-        .send({ status: false, msg: "Url should be present" });
+        .send({ status: false, msg: "Please provide a longUrl into postman" });
     }
-
     if (
-      !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.#?&//=]*)/.test(
-        longUrl
+      !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/.test(
+        longUrl.trim()
       )
     ) {
       return res
         .status(400)
-        .send({ status: false, msg: "Please provide a valid Url" });
+        .send({ status: false, msg: "Please provide a valid longUrl" });
     }
 
-    // Validation for Short Url :
-    if (!shortUrl) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "No shortUrl found, please check again" });
-    }
-
-    let url = await urlModel.findOne({ longUrl }).select({ _id: 0, __v: 0});
-
-    if (url) {
+    let duplicateLongUrl = await GET_ASYNC(`${longUrl}`);
+    let duplicateLongUrlCache = JSON.parse(duplicateLongUrl);
+    if (duplicateLongUrlCache)
       return res
         .status(302)
         .send({
-          status: true,
-          message: "given longUrl already shorted in DB",
-          useThisUrl: url
+          msg: "Already a shortUrl exist with this Url in Cache",
+          urlDetails: duplicateLongUrlCache,
         });
-    } else {
-      let urlDetails = await urlModel.create(data);
-      let result = {
-        urlCode: urlDetails.urlCode,
-        longUrl: urlDetails.longUrl,
-        shortUrl: urlDetails.shortUrl,
-      };
-      return res.status(200).send({ status: true, url:result });
-    }
+
+    let duplicateLongUrlDB = await urlModel.findOne({ longUrl: longUrl });
+    if (duplicateLongUrlDB)
+      return res
+        .status(302)
+        .send({
+          msg: "Already a shortUrl exist with this Url in DB",
+          urlDetails: duplicateLongUrlDB,
+        });
+
+    // Generate ShortUrl :
+    let shortUrl = baseUrl + "/" + urlCode;
+
+    let data = {
+      urlCode: urlCode,
+      longUrl: longUrl,
+      shortUrl: shortUrl,
+    };
+    let urlDetails = await urlModel.create(data);
+
+    let result = {
+      urlCode: urlDetails.urlCode,
+      longUrl: urlDetails.longUrl,
+      shortUrl: urlDetails.shortUrl,
+    };
+    await SET_ASYNC(`${longUrl}`, JSON.stringify(result));
+    return res.status(201).send({ status: true, data: result });
   } catch (error) {
+    console.log(error);
     return res.status(500).send({ status: false, msg: error.message });
   }
 };
 
+
+
+
 const getUrl = async function (req, res) {
   try {
     let urlCode = req.params.urlCode;
-    let cachedProfileData = await GET_ASYNC(`${urlCode}`);
-    if (cachedProfileData) {
-      let datatype = JSON.parse(cachedProfileData);
-      return res.status(302).redirect(datatype.longUrl);
+    let urlFromCache = await GET_ASYNC(`${urlCode}`);
+
+    if (urlFromCache) {
+      return res.status(302).redirect(JSON.parse(urlFromCache));
     } else {
-      const profile = await urlModel.findOne({ urlCode: urlCode });
-      if (profile) {
-        await SET_ASYNC(`${urlCode}`, JSON.stringify(profile));
-        return res.status(302).redirect(profile.longUrl);
-      }else{
-          return res.status(404).send({ status:false,message:"no url found with this urlCode"})
+      let urlFromMongoDB = await urlModel.findOne({ urlCode: urlCode });
+      if (urlFromMongoDB) {
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(urlFromMongoDB.longUrl));
+        return res.status(302).redirect(urlFromMongoDB.longUrl);
+      } else {
+        return res
+          .status(404)
+          .send({ status: false, msg: "No url found with this urlCode" });
       }
     }
   } catch (err) {
-    console.log(err);
-    return res.status(500).send({ status: false, message: err.message });
+    console.log(error);
+    return res
+      .status(500)
+      .status(500)
+      .send({ status: true, message: err.message });
   }
 };
 
-module.exports.urlShortner = urlShortner;
-module.exports.getUrl = getUrl;
+module.exports = {
+  urlShortner,
+  getUrl,
+};
